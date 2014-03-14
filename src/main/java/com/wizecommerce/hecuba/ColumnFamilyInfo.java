70c5bb30a729bbf386038a121ffbb626c40be793
Ankur Singhal <ankur.singhal@wizecommerce.com>
2014-03-14 14:41:17 +0530

    Added methods in HecubaClientManager to retrieve only keys using secondary index columns

--- src/main/java/com/wizecommerce/hecuba/ColumnFamilyInfo.java
+++ src/main/java/com/wizecommerce/hecuba/ColumnFamilyInfo.java
@@ -14,47 +14,58 @@
 
 package com.wizecommerce.hecuba;
 
+import com.google.common.base.Objects;
+
 public class ColumnFamilyInfo {
 
-    private String name;
-    private String comparatorType = "UTF8Type";
-    private String keyValidationClass = "LongType";
-    private String DefaultValidationClass = "UTF8Type";
+	private String name;
+	private String comparatorType = "UTF8Type";
+	private String keyValidationClass = "LongType";
+	private String defaultValidationClass = "UTF8Type";
 
-    public ColumnFamilyInfo(String name) {
-        this.name = name;
-    }
+	public ColumnFamilyInfo(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getComparatorType() {
-        return comparatorType;
-    }
+	public String getComparatorType() {
+		return comparatorType;
+	}
 
-    public void setComparatorType(String coparatorType) {
-        this.comparatorType = coparatorType;
-    }
+	public void setComparatorType(String coparatorType) {
+		this.comparatorType = coparatorType;
+	}
 
-    public String getKeyValidationClass() {
-        return keyValidationClass;
-    }
+	public String getKeyValidationClass() {
+		return keyValidationClass;
+	}
 
-    public void setKeyValidationClass(String keyValidationClass) {
-        this.keyValidationClass = keyValidationClass;
-    }
+	public void setKeyValidationClass(String keyValidationClass) {
+		this.keyValidationClass = keyValidationClass;
+	}
 
-    public String getDefaultValidationClass() {
-        return DefaultValidationClass;
-    }
+	public String getDefaultValidationClass() {
+		return defaultValidationClass;
+	}
 
-    public void setDefaultValidationClass(String defaultValidationClass) {
-        DefaultValidationClass = defaultValidationClass;
-    }
+	public void setDefaultValidationClass(String defaultValidationClass) {
+		this.defaultValidationClass = defaultValidationClass;
+	}
+
+	public String toString() {
+		return Objects.toStringHelper(this)
+				.add("Name", name)
+				.add("ComparatorType", comparatorType)
+				.add("KeyValidationClass", keyValidationClass)
+				.add("DefaultValidationClass", defaultValidationClass)
+				.toString();
+	}
 
 }
