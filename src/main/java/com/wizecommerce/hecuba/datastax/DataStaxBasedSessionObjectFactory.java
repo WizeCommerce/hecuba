598ac2e122add7f705e4dd25005d2cacca049e5c
Anshul Chauhan <anshul.chauhan@wizecommerce.com>
2014-09-19 13:05:34 +0530

    NA-2045 Sharing connections between various instances of HecubaClientManager:
    -After moving to datastax we can connect to multiple column family and key spaces using same session and cluster respectively.
    -It is the recommended way of using the driver.
    -This patch includes changes for cluster and session sharing between multiple HecubaClientManager instances.

---
+++ src/main/java/com/wizecommerce/hecuba/datastax/DataStaxBasedSessionObjectFactory.java
@@ -0,0 +1,154 @@
+package com.wizecommerce.hecuba.datastax;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Cluster.Builder;
+import com.datastax.driver.core.HostDistance;
+import com.datastax.driver.core.PoolingOptions;
+import com.datastax.driver.core.ProtocolOptions.Compression;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SocketOptions;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.datastax.driver.core.policies.LatencyAwarePolicy;
+import com.datastax.driver.core.policies.LoadBalancingPolicy;
+import com.datastax.driver.core.policies.RoundRobinPolicy;
+import com.datastax.driver.core.policies.TokenAwarePolicy;
+import com.wizecommerce.hecuba.datastax.SessionCachingKey.ClusterCachingKey;
+
+/**
+ * This class is to be used for internal purposes of hecuba client. This <b>singleton</b> implementation will
+ * cache cluster and sessions according to datastax driver rules. i.e Use one Cluster instance per (physical)
+ * cluster (per application lifetime). Use at most one Session per keyspace
+ * 
+ * @author anschauhan
+ *
+ */
+@ThreadSafe
+public class DataStaxBasedSessionObjectFactory {
+	private Map<ClusterCachingKey, Cluster> clusterCache = null;
+
+	private Map<SessionCachingKey, Session> sessionCache = null;
+	private Map<SessionCachingKey, AtomicInteger> usersPerSession = null;
+	
+	// Initializing egarly since its very light weight, until clusters and session are built. Guarantees thread safety.
+	private static final DataStaxBasedSessionObjectFactory instance = new DataStaxBasedSessionObjectFactory();
+
+	private DataStaxBasedSessionObjectFactory() {
+		clusterCache = new HashMap<>();
+		sessionCache = new HashMap<>();
+		usersPerSession = new HashMap<>();
+	}
+
+	public static DataStaxBasedSessionObjectFactory getInstance() {
+		return instance;
+	}
+
+	/**
+	 * Call returnSession() everytime getSession() is called.
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Session getSession(SessionCachingKey key) {
+		Session session = sessionCache.get(key);
+		if (session == null) {
+			synchronized (sessionCache) {
+				// Double check locking
+				if (session == null) {
+					Cluster cluster = getCluster(key.getClusterKey());
+					session = cluster.connect(key.getKeySpace());
+					sessionCache.put(key, session);
+					usersPerSession.put(key, new AtomicInteger());
+				}
+			}
+		}
+		usersPerSession.get(key).incrementAndGet();
+		return session;
+	}
+
+	public void returnSession(SessionCachingKey key) {
+		int count = usersPerSession.get(key).decrementAndGet();
+		if (count <= 0) {
+			Session session = sessionCache.remove(key);
+			// use different object to synchronize as this is independent from creation.
+			synchronized (clusterCache) {
+				if (session != null) {
+					session.close();
+				}
+			}
+		}
+	}
+
+	private Cluster getCluster(ClusterCachingKey key) {
+		// No need of synchronizing here as getSession is already synchronized.
+		Cluster cluster = clusterCache.get(key);
+		if (cluster == null) {
+			Map<String, Object> properties = key.getProperties();
+			LoadBalancingPolicy loadBalancingPolicy;
+			if (properties.containsKey("dataCenter")) {
+				loadBalancingPolicy = new DCAwareRoundRobinPolicy((String) properties.get("datacenter"));
+			} else {
+				loadBalancingPolicy = new RoundRobinPolicy();
+			}
+			loadBalancingPolicy = new TokenAwarePolicy(loadBalancingPolicy);
+			loadBalancingPolicy = LatencyAwarePolicy.builder(loadBalancingPolicy).build();
+
+			Builder builder = Cluster.builder().addContactPoints(key.getLocationUrls())
+					.withLoadBalancingPolicy(loadBalancingPolicy);
+
+			if (properties.containsKey("port")) {
+				builder.withPort((Integer) properties.get("port"));
+			}
+
+			if (properties.containsKey("username") && properties.containsKey("password")) {
+				builder.withCredentials((String) properties.get("username"),
+						(String) properties.get("password"));
+			}
+
+			if (properties.containsKey("compressionEnabled")
+					&& (Boolean) properties.get("compressionEnabled")) {
+				builder.withCompression(Compression.LZ4);
+			}
+
+			SocketOptions socketOptions = null;
+			if (properties.containsKey("readTimeout")) {
+				socketOptions = new SocketOptions();
+
+				socketOptions.setReadTimeoutMillis((Integer) properties.get("readTimeout"));
+			}
+
+			if (properties.containsKey("connectTimeout")) {
+				if (socketOptions == null) {
+					socketOptions = new SocketOptions();
+				}
+				socketOptions.setConnectTimeoutMillis((Integer) properties.get("connectTimeout"));
+			}
+
+			if (socketOptions != null) {
+				builder.withSocketOptions(socketOptions);
+			}
+
+			PoolingOptions poolingOptions = null;
+			if (properties.containsKey("maxConnectionsPerHost")) {
+				poolingOptions = new PoolingOptions();
+				Integer maxConnectionsPerHost = (Integer) properties.get("maxConnectionsPerHost");
+				poolingOptions.setMaxConnectionsPerHost(HostDistance.REMOTE, maxConnectionsPerHost);
+				poolingOptions.setMaxConnectionsPerHost(HostDistance.LOCAL, maxConnectionsPerHost);
+			}
+
+			if (poolingOptions != null) {
+				builder.withPoolingOptions(poolingOptions);
+			}
+
+			cluster = builder.build();
+			clusterCache.put(key, cluster);
+		}
+		return cluster;
+	}
+
+}
---
+++ src/main/java/com/wizecommerce/hecuba/datastax/SessionCachingKey.java
@@ -0,0 +1,149 @@
+package com.wizecommerce.hecuba.datastax;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import com.google.common.base.Objects;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+/**
+ * This class is to be used for internal purposes of Hecuba Client. Member variables are kept as generic as
+ * possible such that it can be used as caching key for other cluster pools as well.
+ * 
+ * @author anschauhan
+ *
+ */
+public class SessionCachingKey {
+
+	private ClusterCachingKey clusterKey;
+
+	private String keySpace;
+
+	private Map<String,Object> sessionProperties;
+	public SessionCachingKey(String[] locationUrls, String keySpace) {
+		this(locationUrls, keySpace, null);
+	}
+
+	public SessionCachingKey(String[] locationUrls, String keySpace, Map<String, Object> properties) {
+		this.clusterKey = new ClusterCachingKey(locationUrls, properties);
+		this.keySpace = keySpace;
+	}
+
+	public String getKeySpace() {
+		return keySpace;
+	}
+
+	public void setKeySpace(String keySpace) {
+		this.keySpace = keySpace;
+	}
+
+	public ClusterCachingKey getClusterKey() {
+		return clusterKey;
+	}
+
+	public void setClusterKey(ClusterCachingKey clusterKey) {
+		this.clusterKey = clusterKey;
+	}
+
+	public void setClusterProperties(Map<String, Object> props) {
+		if (props != null) {
+			this.clusterKey.clusterProperties = props;
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		SessionCachingKey other;
+		if (o instanceof SessionCachingKey) {
+			other = (SessionCachingKey) o;
+		} else {
+			return false;
+		}
+
+		return keySpace.equals(other.keySpace) && clusterKey.equals(other.clusterKey);
+	}
+
+	@Override
+	public int hashCode() {
+			return clusterKey.hashCode() + 31 * keySpace.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return Objects.toStringHelper(this).add("Cluster properties: ", clusterKey).add("keySpace", keySpace)
+				.toString();
+	}
+
+	public static class ClusterCachingKey {
+		private String[] locationUrls;
+		/**
+		 * Optional properties. For each property that's different from last one, this key will change and
+		 * will create a new cluster and new session, so input properties carefully. Do not input what's not
+		 * necessary or might be transient or mutable. And since we are not expecting this to be used very
+		 * frequently we are not caching hashCode.
+		 */
+
+		private Map<String, Object> clusterProperties;
+
+		public ClusterCachingKey(String[] locationUrls) {
+			this(locationUrls, null);
+		}
+
+		public ClusterCachingKey(String[] locationUrls, Map<String, Object> properties) {
+			Arrays.sort(locationUrls);
+			this.locationUrls = locationUrls;
+			if (properties == null) {
+				properties = new HashMap<String, Object>();
+			}
+			this.clusterProperties = properties;
+		}
+
+		public void addProperty(String name, Object value) {
+			clusterProperties.put(name, value);
+		}
+
+		public String[] getLocationUrls() {
+			return locationUrls;
+		}
+
+		public void setLocationUrls(String[] locationUrls) {
+			this.locationUrls = locationUrls;
+		}
+
+		public Map<String, Object> getProperties() {
+			return clusterProperties;
+		}
+
+		public void setProperties(Map<String, Object> properties) {
+			this.clusterProperties = properties;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			ClusterCachingKey other;
+			if (o instanceof ClusterCachingKey) {
+				other = (ClusterCachingKey) o;
+			} else {
+				return false;
+			}
+
+			return new EqualsBuilder().append(clusterProperties, other.getProperties())
+					.append(locationUrls, other.getLocationUrls()).isEquals();
+		}
+
+		@Override
+		public int hashCode() {
+			return new HashCodeBuilder().append(clusterProperties).append(locationUrls).toHashCode();
+		}
+
+		@Override
+		public String toString() {
+			return Objects.toStringHelper(this).add("locationUrls", locationUrls)
+					.add("properties", clusterProperties).toString();
+		}
+	}
+}
